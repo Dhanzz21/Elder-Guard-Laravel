8bf775fe67b457452fc6c528681de8a6de7af12c
Dhanzz21 <rmdhani733@gmail.com>
2026-07-23 00:38:48 +0700

    Perbaiki absolute path api index php untuk Vercel

--- api/index.php
+++ api/index.php
@@ -1,4 +1,7 @@
 <?php
 
-// Forward Vercel requests to normal Laravel public folder
+// Tangani direct path untuk lingkungan serverless Vercel
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+
+// Arahkan langsung ke public/index.php Laravel
 require __DIR__ . '/../public/index.php';
